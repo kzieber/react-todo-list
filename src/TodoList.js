e71b6e8280df2d6db0037b5f4abf7cbe3c7940d5
Kevin Zieber <zieber.kevinr@gmail.com>
2021-03-20 19:17:43 -0700

    feature: todos can be edited

--- src/TodoList.js
+++ src/TodoList.js
@@ -11,6 +11,7 @@ export default class TodoList extends Component {
 
     this.create = this.create.bind(this)
     this.remove = this.remove.bind(this)
+    this.updateTodo = this.updateTodo.bind(this)
   }
 
   create(newTodo){
@@ -25,9 +26,19 @@ export default class TodoList extends Component {
     })
   }
 
+  updateTodo(id, updatedTask){
+    const updatedTodos = this.state.todos.map(todo => {
+      if(todo.id === id){
+        return {...todo, task: updatedTask};
+      }
+      return todo;
+    })
+    this.setState({ todos: updatedTodos })
+  }
+
   render() {
     const todoItems = this.state.todos.map(todo => {
-      return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove}/>
+      return <Todo key={todo.id} id={todo.id} task={todo.task} removeTodo={this.remove} updateTodo={this.updateTodo}/>
     })
 
     return (
